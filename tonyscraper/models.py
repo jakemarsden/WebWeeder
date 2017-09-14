aafd469547c33b44a947b2601b9d22800732f1e7
jakemarsden <jakemarsdenjm@gmail.com>
2017-09-14 19:51:56 +1200

    Continuing domain configuration to make crawler dynamic

---
+++ tonyscraper/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+class PageMetadata:
+    def __init__(self):
+        self.domain: str = None
+        self.url: str = None
+        self.url_date: datetime = None
+        self.page_title: str = None
+        self.article_title: str = None
+        self.article_date: str = None
+        self.directory: str = None
+        self.file_metadata: str = None
+        self.file_raw_html: str = None
+        self.file_article_html: str = None
+        self.file_article_plaintext = None
