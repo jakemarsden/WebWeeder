2bef75d563f16c460c773f31065ec1e9d512d5db
jakemarsden <jakemarsdenjm@gmail.com>
2017-09-15 22:56:05 +1200

    Fuck regular expressions

--- config.py
+++ config.py
@@ -31,7 +31,18 @@ DOMAINS: List[DomainConfig] = [
                        article_date_selector='.post-meta div.date > a',
                        article_content_selector='div.article-content'),
     SimpleDomainConfig(name='clashdaily.com',
-                       url_patterns=[r'^https?://(www\.)?clashdaily.com/[0-9]{4}/[0-9]{2}/[^/]+/?$'],
+                       # If you're here because this RegEx is broken, the best thing to do is delete all traces of the
+                       # application, cut ties with everyone you know and begin a new life somewhere far, far away.
+                       #
+                       # "^" matches the start of the string
+                       # "https?" matches both "http" and "https"
+                       # "(www\.)?" matches both "" and "www."
+                       # "[0-9]{4}" matches 4 consecutive digits
+                       # "[^/]+" matches one or more characters which aren't "/"
+                       # "(?<!/[0-9]{2})/?" matches (both "" and "/"), unless it is preceded by "/[0-9]{2}"
+                       # "$" matches the end of the string
+                       #
+                       url_patterns=[r'^https?://(www\.)?clashdaily.com/[0-9]{4}/[0-9]{2}/[^/]+(?<!/[0-9]{2})/?$'],
                        # Seed with each daily summary page
                        seed_urls=['https://clashdaily.com/%04d/%02d/%02d/' % (d.year, d.month, d.day) for d in _DATES],
                        article_title_selector='h1.wpdev-single-title',
