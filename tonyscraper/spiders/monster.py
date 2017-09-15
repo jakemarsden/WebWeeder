f3380b97e0faa6ae05ef3552d8559d1e4af5cd4a
jakemarsden <jakemarsdenjm@gmail.com>
2017-09-15 14:44:43 +1200

    Optimise imports

--- tonyscraper/spiders/monster.py
+++ tonyscraper/spiders/monster.py
@@ -2,7 +2,7 @@ import json
 import os
 import traceback
 from datetime import datetime
-from typing import Optional, Callable
+from typing import Callable, Optional
 
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_file_path
