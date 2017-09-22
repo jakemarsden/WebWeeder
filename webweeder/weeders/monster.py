28980f0210bf9bb2e989a36d78c09e82b39eb92e
jakemarsden <jakemarsdenjm@gmail.com>
2017-09-22 22:58:37 +1200

    Major refactor for weeding - Split finding metadata out of actual weeding code - Will help with error handling, logging in future

--- webweeder/weeders/monster.py
+++ webweeder/weeders/monster.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Iterable, List, Optional
 
 from bs4 import BeautifulSoup
 
@@ -14,10 +14,29 @@ class MonsterWeeder:
     # TODO: better logging, log each page as we process it or something
     # TODO: better error handling, one screwy HTML file shouldn't prevent weeding the rest
 
-    def weed(self):
-        self._weed_subdirectory(config.OUTPUT_DIRECTORY)
+    def find_page_metadatas(self, directory: str) -> Iterable[str]:
+        """
+        :param directory: Where to look for metadata files
+        :return: List of paths for the metadata files of each found page in the given directory, recursively
+        """
+        found_pages: List[str] = []
+        for item_name in os.listdir(directory):
+            item_path = os.path.join(directory, item_name)
+            if os.path.isfile(item_path):
+                if item_name == 'metadata.json':
+                    found_pages.append(item_path)
+            elif os.path.isdir(item_path):
+                found_pages += self.find_page_metadatas(item_path)
+            else:
+                raise TypeError('Not a file or a directory: %s' % item_path)
+        return found_pages
 
-    def _weed_file(self, file: str):
+    def weed_page(self, base_dir: str, file: str):
+        """
+        :param base_dir: Top-level directory containing the raw HTML and plaintext files referenced by the metadata
+        :param file: Full path to the metadata (i.e. already joined to base_dir)
+        :return:
+        """
         # Read and parse JSON file
         json_str = utils.read_text_file(file, missing_ok=False)
         meta: PageMetadata = page_metadata_from_json(json_str)
@@ -26,25 +45,17 @@ class MonsterWeeder:
         if domain_config is None:
             raise RuntimeError('Unconfigured domain "%s" for page: %r' % (meta.domain, meta))
 
-        raw_html_path = os.path.join(config.OUTPUT_DIRECTORY, meta.directory, meta.file_raw_html)
+        # Read raw HTML for the page
+        raw_html_path = os.path.join(base_dir, meta.directory, meta.file_raw_html)
         raw_html = utils.read_text_file(raw_html_path, missing_ok=False)
 
+        # Parse HTML and extract plaintext from article
         soup = BeautifulSoup(raw_html, config.HTML_PARSER)
         plaintext = self._extract_article_content(soup, domain_config)
 
-        plaintext_path = os.path.join(config.OUTPUT_DIRECTORY, meta.directory, meta.file_article_plaintext)
+        # Write plaintext to file
+        plaintext_path = os.path.join(base_dir, meta.directory, meta.file_article_plaintext)
         utils.write_text_file(plaintext_path, plaintext)
-
-    def _weed_subdirectory(self, dir: str):
-        for item_name in os.listdir(dir):
-            item_path = os.path.join(dir, item_name)
-            if os.path.isfile(item_path):
-                if item_name == 'metadata.json':
-                    self._weed_file(item_path)
-            elif os.path.isdir(item_path):
-                self._weed_subdirectory(item_path)
-            else:
-                raise TypeError('Not a file or a directory: %s' % item_path)
 
     @staticmethod
     def _extract_article_content(soup: BeautifulSoup, domain: DomainConfig) -> Optional[str]:
